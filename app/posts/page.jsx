d3de6438ef7f3142283b629543c91f655b7382be
sergio15dev <sergio15dev@gmail.com>
2023-09-11 17:48:02 -0500

    add styles with tailwind css

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,4 +1,5 @@
 import PostCard from "@/components/PostCard";
+import "./Posts.css";
 
 async function loadPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -10,7 +11,7 @@ async function PostsPage() {
   const posts = await loadPosts();
   console.log(posts);
   return (
-    <div>
+    <div className="grid">
       {posts.map((post) => (
         <PostCard post={post} key={post.id} />
       ))}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
-      <h1>Navbar</h1>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
-      <li>
-        <Link href="/tienda">Tienda</Link>
-      </li>
-      <li>
-        <Link href="/posts">Posts</Link>
-      </li>
+    <nav className="py-5 navbar">
+      <Link href="/">
+        <h1 className="text-3xl font-bold">Next Js Example</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/tienda">Tienda</Link>
+        </li>
+        <li>
+          <Link href="/posts">Posts</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
